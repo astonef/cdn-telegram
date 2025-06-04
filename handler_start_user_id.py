5af239fdcda7b48d8a8ff04f34a8623d1f1f0360
stefano <stefano.g.magni@hotmail.it>
2025-06-04 16:33:05 +0200

    Corretto import con update

--- handler_start_user_id.py
+++ handler_start_user_id.py
@@ -1,4 +1,5 @@
 from telegram.ext import CommandHandler
+from telegram import Update
 
 async def handle_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
